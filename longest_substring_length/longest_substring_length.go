a33c7ea307a8148a33df5f9f14728e16d8ab1fd7
luciano.alves <luciano.alves@avansys.com.br>
2022-11-17 16:41:55 -0300

    [main] Fixing logic.

--- longest_substring_length/longest_substring_length.go
+++ longest_substring_length/longest_substring_length.go
@@ -10,7 +10,7 @@ import (
  * Youtube link: https://www.youtube.com/watch?v=Pe64ee24MLY&t=492s
  */
 func main() {
-	s := "abccd  efghijabc"
+	s := "pwwkewaaaa"
 	l := findLongestSubstringLength(s)
 	fmt.Printf("The longest substring with unique characters is: %v.\n", l)
 }
@@ -18,14 +18,18 @@ func main() {
 func findLongestSubstringLength(s string) int {
 	sub := ""
 	sublen := 0
-	for i := 0; i < len(s); i++ {
-		if strings.Count(sub, s[i:i+1]) == 0 {
-			sub += s[i : i+1]
-		} else {
-			sub = s[i : i+1]
-		}
-		if sublen < len(sub) {
-			sublen = len(sub)
+	for leftIndex := 0; leftIndex < len(s); leftIndex++ {
+		rightIndex := leftIndex
+		for ; rightIndex < len(s); rightIndex++ {
+			if strings.Count(sub, s[rightIndex:rightIndex+1]) == 0 {
+				sub = s[leftIndex : rightIndex+1]
+				if sublen < len(sub) {
+					sublen = len(sub)
+				}
+			} else {
+				sub = ""
+				break
+			}
 		}
 	}
 	return sublen
